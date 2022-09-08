e65ff46bf674a2360d7a0d4ff518a3a3e107929d
Kaito Dogi <kendobu0405@gmail.com>
2022-09-08 22:51:34 +0900

    review: MainActivity

--- app/src/main/java/app/kawamura/kawachi/la_count3/MainActivity.kt
+++ app/src/main/java/app/kawamura/kawachi/la_count3/MainActivity.kt
@@ -11,6 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+        // Doggy: Button が1つしかないので、button2 ではなく button という id 名にすると良さそう！
+        //  ただ、 button という名前だと何の button か分からないので
+        //  tutorial_button など、押したらどのような処理をするボタンか分かりやすくしても良さそう！
         binding.button2.setOnClickListener {
             val toNextActivityIntent = Intent(this, TutorialActivity::class.java)
             startActivity(toNextActivityIntent)
