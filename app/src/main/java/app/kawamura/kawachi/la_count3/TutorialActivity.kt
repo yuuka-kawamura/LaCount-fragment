83369151537a99ac7b5c7162a7c0be479bb4a2de
Kaito Dogi <kendobu0405@gmail.com>
2022-09-08 22:41:20 +0900

    review: TutorialActivity

--- app/src/main/java/app/kawamura/kawachi/la_count3/TutorialActivity.kt
+++ app/src/main/java/app/kawamura/kawachi/la_count3/TutorialActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
 import app.kawamura.kawachi.la_count3.databinding.ActivityTutorialBinding
 
+// Doggy: アプリを起動して、 TutorialActivity を最初に表示する方法を調べてみよう！
 class TutorialActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTutorialBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +18,10 @@ class TutorialActivity : AppCompatActivity() {
         binding.viewPager.adapter = ViewPagerAdapter(this)
         binding.viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
+        // Doggy: 使用していない変数は削除しよう！
         val maxItemCount: Int = binding.viewPager.adapter?.itemCount ?: 0
 
+        // Doggy: 使用していない処理は削除しよう！
         binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
