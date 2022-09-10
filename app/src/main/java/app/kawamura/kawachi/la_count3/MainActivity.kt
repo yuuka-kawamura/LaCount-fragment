6d15580e9a55ab222b3495bc9f58a03b0e605405
risako070310 <risako070310@gmail.com>
2022-09-11 04:41:18 +0900

    :speech_balloon: review: MainActivity.kt

--- app/src/main/java/app/kawamura/kawachi/la_count3/MainActivity.kt
+++ app/src/main/java/app/kawamura/kawachi/la_count3/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import app.kawamura.kawachi.la_count3.databinding.ActivityMainBinding
 
+//reviewed by toppo 🧸: MainActivityじゃなく、TutorialActivityがアプリ初期起動の画面になるようにmanifestsの設定をしてみよう！
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //🧸: viewBindingが使えていてGood
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
         binding.button2.setOnClickListener {
             val toNextActivityIntent = Intent(this, TutorialActivity::class.java)
@@ -18,4 +20,5 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    //🧸: 余分な空白行を消そう！コードが長くなった時にも読みやすくなるよ
 }
