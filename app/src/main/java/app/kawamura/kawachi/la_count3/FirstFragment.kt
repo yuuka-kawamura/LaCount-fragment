34fcc94d7af4636aae77949588c5bc24d5095c31
risako070310 <risako070310@gmail.com>
2022-09-11 04:45:10 +0900

    :speech_balloon: review: FirstFragment.kt

--- app/src/main/java/app/kawamura/kawachi/la_count3/FirstFragment.kt
+++ app/src/main/java/app/kawamura/kawachi/la_count3/FirstFragment.kt
@@ -2,5 +2,6 @@ package app.kawamura.kawachi.la_count3
 
 import androidx.fragment.app.Fragment
 
+//reviewed by toppo 🧸: 余分なFragmentの自動生成コードが消えていてGood。ファイルの命名もわかりやすくていいね！
 class FirstFragment:Fragment(R.layout.fragment_first) {
 }
